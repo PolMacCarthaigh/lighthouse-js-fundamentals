446a243f636256b1f1bd58b9e85fb9921eb99823
Paul McCarthy <paulmccarthy2010@live.ca>
2016-09-30 02:11:30 +0000

    loopy-lighthouse-2

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,10 +1,14 @@
-for (var i = 100; i < 201; i++) {
-  var output = '';
-  if (i % 3 === 0) {
-    output += "Loopy";
+var loopyLighthouse = function(range, multiples, words) {
+  for (var number = range[0]; number <= range[1]; number++) {
+    if (number % multiples[0] === 0 && number % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (number % multiples[1] === 0) {
+      console.log(words[1]);
+    } else if (number % multiples[0] === 0) {
+      console.log(words[0]);
+    } else {
+      console.log(number);
+    }
   }
-  if (i % 4 === 0) {
-    output += "Lighthouse";
-  }
-  console.log(output === '' ? i : output);
 }
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
